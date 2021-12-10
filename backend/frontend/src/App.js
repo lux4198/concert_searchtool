823cdf1b0453c976c1816371e2d451179004ab1e
Lukas Nittka <lukas.nittka@gmail.com>
2021-12-10 16:57:51 +0100

    app now displays api data with an accordion for each concert

--- backend/frontend/src/App.js
+++ backend/frontend/src/App.js
@@ -1,11 +1,104 @@
+import axios from 'axios'
+import {useEffect, useState} from 'react'
 
+import {Typography, Accordion, AccordionDetails, AccordionSummary} from '@mui/material'
+
+import {ExpandMore} from '@mui/icons-material'
+
+import moment from 'moment'
+
+
+function Accordionobject(props){
+  const concert = props.concert
+
+  let formattedDate = moment(concert.date).format('DD. MMM YYYY')
+  let formattedDateTime = moment.utc(concert.date).format('DD. MMM YYYY - HH:mm')
+
+  return(
+
+  <Accordion> 
+    <AccordionSummary
+    expandIcon={<ExpandMore/>}
+    aria-controls="panel1a-content"
+    id="panel1a-header"
+    >
+      <Typography style = {{width : '15%'}}>{formattedDate}</Typography>
+      <Typography style = {{ color: '#526268' }}>{props.subheader}</Typography>
+    </AccordionSummary>
+    <AccordionDetails style = {{display : 'flex', flexDirection : 'column',  }}></AccordionDetails>
+    <table style = {{width : '100%'}}>
+            <tbody style = {{textAlign : 'left', }}>
+              <tr>
+                  <th>Date</th>
+                  <td>{formattedDateTime}</td>
+              </tr>
+              <tr>
+                  <th>Ensemble</th>
+                  <td>{concert.location} </td>
+              </tr>
+              <tr>
+                  <th>Conductor</th>
+                  <td>{concert.conductor} </td>
+              </tr>
+              <tr>
+                  <th>Artists</th>
+                  <td>{concert.artists} </td>
+              </tr>
+              <tr>
+                  <th>Pieces</th>
+                  <td>{concert.pieces}</td> 
+              </tr>
+            </tbody>
+    </table>
+  </Accordion>
+  )
+}
+
+
+function ConcertDisplay(props){
+
+  const concerts = props.concerts
+
+  if (props.concerts.length > 0){
+    return(
+        concerts.map((concert) => 
+          <Accordionobject header = {concert.date} subheader = {concert.location + '  -  ' + concert.conductor} concert = {concert} /> 
+        
+        )
+    )
+  }
+  else {
+    return(
+      <div>
+        Hello There
+      </div>
+  )
+}
+}
 
 function App() {
-  return (
-    <div>
-      Hello There
-    </div>
-  );
+
+  useEffect(() => {
+    getAllConcerts();
+  }, []);
+
+  const [concerts, getConcerts] = useState('');
+
+  const getAllConcerts = () => {
+    axios.get('/api/events')
+    .then((response) => {
+      const allConcerts = response.data
+      getConcerts(allConcerts)
+    })
+  }
+
+    return(
+      <div>
+        <ConcertDisplay concerts = {concerts}/>
+      </div>
+    )
 }
+
+
 
 export default App;
