37254e114eaee6a63e2603115de68c4ba0b61827
Lukas Nittka <lukas.nittka@gmail.com>
2021-12-16 21:30:28 +0100

    webscraper now gets data from details page for each concert including pieces and composers

--- backend/searchtool/webscraper.py
+++ backend/searchtool/webscraper.py
@@ -8,9 +8,17 @@ setlocale(LC_TIME, 'de_DE')
 
 # from .models import Event 
 
+def get_concert_details(details_link):
+    data = requests.get(details_link)
+    soup = BeautifulSoup(data.text, 'html.parser')
+
+    return [(item.contents[0].text, item.contents[1].text) for item in soup.find_all('div', {'class' : 'piece'})]   
+
+
 
 def main():
     month_list = ['2021-12', '2022-01', '2022-02', '2022-03', '2022-04', '2022-05', '2022-06', '2022-07']
+    month_list = ['2022-03']
 
     for month in month_list:
         print(month, '\n')
@@ -18,58 +26,73 @@ def main():
         data = requests.get('https://www.berliner-philharmoniker.de/konzerte/kalender/von/' + month)
         soup = BeautifulSoup(data.text, 'html.parser')
 
-        event = []
-
-        for element in soup.find_all('article', { 'class' : 'calendar-entry clickable-box orchestra'}):
-            
-            concert = element.find_all('div', {'class' : 'performance-details-wrapper'})
-            concert_date = element.find('div', {'class' : 'performance-date'})
-            musicians = element.find_all('h2', {'class' : 'main-musician'})
-
-            concert_date = ' '.join(concert_date.text.split(' ')[:-1])
-            concert_date = datetime.strptime(concert_date, '%A,%d. %b %Y,  %H.%M')
-
-            event.append({'concert_general' : concert, 'date' : concert_date, 'musicians' : musicians})
-
-
         concerts = []
 
-        for element in event:
+        # find each concert in concert list 
+        for element in soup.find_all('article', { 'class' : 'calendar-entry clickable-box orchestra'}):
+            
+            # get details from each concert 
+            concert_date = element.find('div', {'class' : 'performance-date'})
+
+            musicians = element.find_all(['h2', 'h3'], {'class' : ['main-musician', 'other-musician']})
+
+            # link that leads to specific concert 
+            details_link = element.find('a', {'class' : 'button grey read-more action-item'}, href = True)
+            details_link = 'https://www.berliner-philharmoniker.de/' + details_link['href']
+
+            print(get_concert_details(details_link))
+
+            # create datetime objects from concert_date - sample: Samstag,08. Jan 2022, 19.00 Uhr
+            concert_date = ' '.join(concert_date.text.split(' ')[:-1])
+
+            # convert Mär to Mrz so strptime can parse data for march
+            if concert_date.split(' ')[1] != 'Mär':
+                concert_date = datetime.strptime(concert_date, '%A,%d. %b %Y,  %H.%M')
+            else:
+                concert_date = concert_date.split(' ')
+                concert_date[1] = 'Mrz'
+                concert_date = ' '.join(concert_date)
+                concert_date = datetime.strptime(concert_date, '%A,%d. %b %Y,  %H.%M')
 
             singleevent = {}
 
-            singleevent['date'] = element['date']
+            singleevent['date'] = concert_date
+            
+            # get ensemble name and pieces from concert element 
 
-            for div in element['concert_general']:      
-                info = [txt.text for txt in div]    
 
-                singleevent['ensemble'] = info[0]
-                singleevent['pieces'] = info[-1]
 
+            # get musicians / orchester from musicians element 
             singleevent['musicians'] = {}
-            for div in element['musicians']:
+            for div in musicians:
                 musician = div.contents[0]
                 role = div.find('span', {'class' : 'role'})
-                
+
                 if not role:
-                    singleevent['musicians'][musician] = 'Orchester'
+                    singleevent['musicians'][musician] = musician
                 else:
+                    if not role.contents:
+                        singleevent['musicians'][musician] = musician
+                        continue
                     singleevent['musicians'][musician] = role.contents[0]
 
+            # define the city of the events 
             singleevent['city'] = 'Berlin'
 
+            # create entries in database for scraped data 
             # Event.objects.create(date = singleevent['date'], 
-            #     location = singleevent['location'],
+            #     city = singleevent['city'],
             #     conductor = singleevent['conductor'], 
-            #     artists = singleevent['artists'], 
-            #     pieces = singleevent['pieces'])
+            #     musicians = singleevent['musicians'], 
+            #     pieces = singleevent['pieces'],
+            #     ensemble = singleevent['ensemble'])
 
             
 
             concerts.append(singleevent)
 
-        for item in concerts:
-            print(item, '\n')
+        # for item in concerts:
+        #     print(item, '\n')
 
 
 if __name__ == '__main__':
