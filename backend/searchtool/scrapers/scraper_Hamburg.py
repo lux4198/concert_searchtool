b32d29601294ca70e125c791add0e52bf689bca5
Lukas Nittka <lukas.nittka@gmail.com>
2022-01-02 19:32:48 +0100

    scraper extracts datetime from header

--- backend/searchtool/scrapers/scraper_Hamburg.py
+++ backend/searchtool/scrapers/scraper_Hamburg.py
@@ -4,9 +4,17 @@ from bs4 import BeautifulSoup
 data = requests.get('https://www.elbphilharmonie.de/de/programm/EPHH/OR/')
 soup = BeautifulSoup(data.text, 'html.parser')
 
+
 for event in soup.find_all('div', {'class' : 'grid-x grid-margin-x'}):
 
     # get link to each specific concert for details 
     concert_link = event.find('a', href = True)
     concert_link = 'https://www.elbphilharmonie.de/' + concert_link['href']
+    
+    data = requests.get(concert_link)
+    soup = BeautifulSoup(data.text, 'html.parser')
+
+    # get datetime of concert from header
+    date = soup.find('time')['datetime']
+
     
