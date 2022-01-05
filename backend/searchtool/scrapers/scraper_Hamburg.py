fba77315cc1827307d2704425473f4bf2ab9810c
Lukas Nittka <lukas.nittka@gmail.com>
2022-01-05 17:44:22 +0100

    scraper now looks for concerts until the end of the current season

--- backend/searchtool/scrapers/scraper_Hamburg.py
+++ backend/searchtool/scrapers/scraper_Hamburg.py
@@ -1,69 +1,95 @@
-import requests, re
+import requests
 from bs4 import BeautifulSoup
 
-data = requests.get('https://www.elbphilharmonie.de/de/programm/EPHH/OR/')
-soup = BeautifulSoup(data.text, 'html.parser')
-
 concerts = []
-for event in soup.find_all('div', {'class' : 'grid-x grid-margin-x'}):
 
-    singleevent = {}
+for i in range(100):
+    if i == 0:
+        data = requests.get('https://www.elbphilharmonie.de/de/programm/EPHH/OR/')
+    else: 
+        data = requests.get(concerts[-1]['link'])
+        # if last possible date of concert season is reached, loop breaks 
+        if concerts[-1]['link'] == 'https://www.elbphilharmonie.de/de/programm/14-06-2022/EPHH/OR/':
+            # del(concerts[-1])
+            break
+        # delete last item in concert list (it will be added in the next loop)
+        del(concerts[-1])
 
-    # get link to each specific concert for details 
-    concert_link = event.find('a', href = True)
-    concert_link = 'https://www.elbphilharmonie.de/' + concert_link['href']
-    
-    # get data from details link 
-    data = requests.get(concert_link)
     soup = BeautifulSoup(data.text, 'html.parser')
 
-    # get datetime of concert from header
-    date = soup.find('time')['datetime']
-    singleevent['datetime'] = date
+    for event in soup.find_all('div', {'class' : 'grid-x grid-margin-x'}):
 
-    # get musicians and roles 
-    singleevent['musicians'] = {}
-    musicians = soup.find_all('p', {'class' : 'artists without-space'})
+        singleevent = {}
 
-    for musician in musicians:
-        # musician / conductor always highlighted in bold text
-        Musician = musician.find('b').text
+        # get link to each specific concert for details 
+        concert_link = event.find('a', href = True)
+        concert_link = 'https://www.elbphilharmonie.de/' + concert_link['href']
+        singleevent['link'] = concert_link
+        
+        # get data from details link 
+        data = requests.get(concert_link)
+        soup = BeautifulSoup(data.text, 'html.parser')
 
-        # other text in div is musicians role 
-        for role in musician.contents:
-            if role.text != Musician: 
-                Role = role.get_text(strip = True)
-        # add musicians + role to each event + add conductor as special key 
-        if Role == 'Dirigent':
-            singleevent['conductor'] = Musician
-        singleevent['musicians'][Musician] = Role
-    singleevent['ensemble'] = [musician.find('b').text for musician in musicians][0]
-    
+        # get datetime of concert from header
+        date = soup.find('time')['datetime']
+        singleevent['datetime'] = date
 
-    # get Pieces and Composers 
-    # finds the div that contains this concerts program 
-    for div in soup.find_all('div', {'class' : 'cell medium-6'})[:3]:
-        content = [content.get_text(strip = True) for content in div.contents if content.get_text(strip = True) != '']
-        if content[0] != 'Programm':
-            continue
-        else:
-            composers_pieces = div
+        # get musicians and roles 
+        singleevent['musicians'] = {}
+        musicians = soup.find_all('p', {'class' : 'artists without-space'})
 
-    # gets the paragraphs with each composer + pieces from above mentioned div
-    paragraphs = [paragraphs for paragraphs in composers_pieces.find_all('p')]
-    
-    # long list comprehension -> checks every paragraph in composers_pieces div -> extracts the text and strips it of line breaks etc
-    # -> does not accept empty strings or -pause-, if paragraph does not contain program, but e.g. 'Einfuehrung', it is not accepted
-    composers_and_pieces = [[piece.get_text(strip = True) for piece in paragraph.contents if piece.get_text(strip = True) != '' 
-                and piece.get_text(strip = True) != '- Pause -'] for paragraph in paragraphs if paragraph.contents[1].get_text(strip = True) != 'Einführung']
+        if musicians:
+            for musician in musicians:
+                # musician / conductor always highlighted in bold text
+                Musician = musician.find('b').text
 
-    # extracts composers and pieces from the program 
-    composers = [composers[0] for composers in composers_and_pieces if composers]
-    pieces = [pieces[1:] for pieces in composers_and_pieces if pieces]
+                # other text in div is musicians role 
+                for role in musician.contents:
+                    if role.text != Musician: 
+                        Role = role.get_text(strip = True)
+                # add musicians + role to each event + add conductor as special key 
+                if Role == 'Dirigent':
+                    singleevent['conductor'] = Musician
+                singleevent['musicians'][Musician] = Role
+            singleevent['ensemble'] = [musician.find('b').text for musician in musicians][0]        
 
-    singleevent['composers'] = composers
-    singleevent['pieces'] = pieces 
+        # get Pieces and Composers 
+        # finds the div that contains this concerts program 
+        for div in soup.find_all('div', {'class' : 'cell medium-6'})[:3]:
+            content = [content.get_text(strip = True) for content in div.contents if content.get_text(strip = True) != '']
+            if content[0] != 'Programm':
+                continue
+            else:
+                composers_pieces = div
 
-    print(singleevent, '\n')
+        # gets the paragraphs with each composer + pieces from above mentioned div
+        paragraphs = [paragraphs for paragraphs in composers_pieces.find_all('p')]
+        
+        # long list comprehension -> checks every paragraph in composers_pieces div -> extracts the text and strips it of line breaks etc
+        # -> does not accept empty strings or -pause-, if paragraph does not contain program, but e.g. 'Einfuehrung', it is not accepted
+        composers_and_pieces = []
+        for paragraph in paragraphs:
+            if len(paragraph.contents) >= 2 and paragraph.contents[1].get_text(strip = True) != 'Einführung':
+                ls = [piece.get_text(strip = True) for piece in paragraph.contents if piece.get_text(strip = True) != '' and piece.get_text(strip = True) != '- Pause -']
+            elif len(paragraph.contents) <= 2 and paragraph.contents[0].get_text(strip = True) != 'Einführung':
+                ls = [paragraph.get_text(strip = True)]
+            composers_and_pieces.append(ls)
 
+        # extracts composers and pieces from the program 
+        composers = [composers[0] for composers in composers_and_pieces if composers]
+        pieces = [pieces[1:] for pieces in composers_and_pieces if pieces]
+
+        singleevent['composers'] = composers
+        singleevent['pieces'] = pieces 
+
+        # if no composer is mentioned, all of the text is put into the pieces list (e.g. for piano recitals)
+        if len(composers_and_pieces) == 1:
+            singleevent['pieces'] = composers
+            singleevent['composers'] = []
+
+        concerts.append(singleevent)
+        
+        # print(singleevent, '\n')
+
+print(concerts[43:45])
     
