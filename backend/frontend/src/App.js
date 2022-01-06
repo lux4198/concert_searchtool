119613778c008a448913aaf856c40f767be6eb85
Lukas Nittka <lukas.nittka@gmail.com>
2022-01-06 18:13:40 +0100

    now only future concerts are displayed

--- backend/frontend/src/App.js
+++ backend/frontend/src/App.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import {React, useEffect, useState} from 'react'
+import moment from 'moment';
 
 import ConcertItem from "./components/ConcertItem.js";
 
@@ -7,13 +8,14 @@ import ConcertItem from "./components/ConcertItem.js";
 function ConcertDisplay(props){
 
   const concerts = props.concerts
+  const now = moment()
 
   if (props.concerts.length > 0){
     return(
         concerts.map((concert) => 
+          moment(concert.date) > now &&
           <ConcertItem key = {concert.id} header = {concert.date} subheader = {concert.ensemble + '  -  ' + concert.conductor} concert = {concert} /> 
-        
-        )
+    )
     )
   }
   else {
