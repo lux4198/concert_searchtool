6abd0bb38e102212da868a05d0f7a65ebfbe3975
Lukas Nittka <lukas.nittka@gmail.com>
2022-01-14 23:35:23 +0100

    Munich is now a selectable city under searchspecifications

--- backend/frontend/src/components/SearchSpecification.js
+++ backend/frontend/src/components/SearchSpecification.js
@@ -5,10 +5,10 @@ import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 function SearchSpecification(props) {
     const [city, setCity] = useState('');
-    const [checked, setChecked] = useState([false, false])
+    const [checked, setChecked] = useState([false, false, false])
 
     const onChange1 = (event) => {
-        setChecked([event.target.checked, checked[1]]);
+        setChecked([event.target.checked, checked[1], checked[2]]);
 
         if (event.target.checked){
             setCity(city + ',Berlin'); 
@@ -21,15 +21,26 @@ function SearchSpecification(props) {
     } 
 
     const onChange2 = (event) => {
-        setChecked([checked[0], event.target.checked]);
+        setChecked([checked[0], event.target.checked, checked[2]]);
 
         if (event.target.checked){
             setCity(city + ',Hamburg'); 
             props.onClick(city + ',Hamburg')}
-
         else {
             setCity(city.replace(',Hamburg', '')); 
             props.onClick(city.replace(',Hamburg', ''))
+        } 
+    } 
+
+    const onChange3 = (event) => {
+        setChecked([checked[0], checked[1], event.target.checked]);
+
+        if (event.target.checked){
+            setCity(city + ',Munich'); 
+            props.onClick(city + ',Munich')}
+        else {
+            setCity(city.replace(',Munich', '')); 
+            props.onClick(city.replace(',Munich', ''))
         } 
     } 
     return (
@@ -44,6 +55,8 @@ function SearchSpecification(props) {
                             label = 'Berlin'/>
                             <FormControlLabel control = {<Checkbox onClick = {onChange2}/>}
                             label = 'Hamburg'/>
+                            <FormControlLabel control = {<Checkbox onClick = {onChange3}/>}
+                            label = 'Munich'/>
                     </FormGroup>
                     
                 </AccordionDetails>
