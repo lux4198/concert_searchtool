470fdcc4687db73bc3b48e8a2bed2bc489b4eb09
Lukas Nittka <lukas.nittka@gmail.com>
2022-01-20 13:30:57 +0100

    searchbar now has autocomplete function that suggests search from all concert items

--- backend/frontend/src/App.js
+++ backend/frontend/src/App.js
@@ -14,11 +14,10 @@ import { Button } from '@material-ui/core';
 function ConcertDisplay(props){
   
   const concerts = props.concerts
-  // const index = props.index
 
   if (props.concerts.length > 0){
     return(
-        concerts./* slice(0,props.index) */map((concert) => 
+        concerts.map((concert) => 
 
         <ConcertItem key = {concert.id} header = {concert.date} subheader = {concert.ensemble + '  -  ' + concert.conductor}
         concert = {concert} /> 
@@ -39,6 +38,7 @@ class App extends Component {
   constructor(){
     super();
       this.state = {
+        allConcerts : [], 
         concerts : '', 
         inputText : '', 
         city: 'city=', 
@@ -64,6 +64,7 @@ class App extends Component {
   
 
   componentDidMount(){
+    this.getAllConcerts(this.state.city + '&' + this.state.inputText, true)
     this.getAllConcerts(this.state.city + '&' + this.state.inputText)
     window.addEventListener('scroll', this.handleScroll);
   }
@@ -82,15 +83,27 @@ class App extends Component {
      }
   }
 
-  getAllConcerts = (input) => {
+  getAllConcerts = (input, didMount) => {
+    // (didMount === undefined)? didMount = false : didMount = true
+
     const date = 'date=' + moment(this.state.date).format('YYYY-MM-DD HH:mm')
-    const index = 'n=' + this.state.index
+
+    if (didMount === true){
+      axios.get('/api/events/?'+ date + '&' + input)
+    .then((response) => {
+      this.setState(() => {return ({allConcerts : response.data})})
+      // console.log(response.data, this.state.search)
+    })
+    }
+
+    else {const index = 'n=' + this.state.index
 
     axios.get('/api/events/?'+ index + '&' + date + '&' + input)
     .then((response) => {
       this.setState(() => {return ({concerts : response.data})})
       // console.log(response.data, this.state.search)
     })
+  }
   };
 
   searchSubmit = (e) => 
@@ -102,15 +115,19 @@ class App extends Component {
     render(){
       return(
         <div>  
-            <Searchbar onSubmit = {this.searchSubmit} onChange = {(e) => {this.setState({inputText : 'q=' + e.target.value})}}/>
+            <Searchbar concerts = {this.state.allConcerts} onSubmit = {this.searchSubmit} onChange = {(e) => {this.setState({inputText : 'q=' + e.target.value})}}
+                        /* value = {} *//>
+
             <SearchSpecification onClick = {(text) => {this.getAllConcerts('city=' + text + '&' + this.state.inputText); this.setState({city : text});}}
             query = {this.state.inputText} reset = {this.state.reset} handleReset = {() => {this.setState({reset : false})}} date = {this.state.date}/>
-
-            <Datepicker value = {this.state.date} onChange = {(newDate) => {this.setState({date : newDate}, () => {this.getAllConcerts(this.state.city + '&' + this.state.inputText)})}}/>
-            <CurrentFilters date = {this.state.date} city = {this.state.city} onClick = {() => {
-                this.setState({city : 'city=', date : new Date, reset : true}, () => {this.getAllConcerts(this.state.inputText)})}}/>
-
-            <ConcertDisplay concerts = {this.state.concerts} date = {this.state.date} /* index = {this.state.index} *//>
+            
+            <div style = {{'display' : 'flex', 'flexDirection' : 'row',}}>
+              <Datepicker value = {this.state.date} onChange = {(newDate) => {this.setState({date : newDate}, () => {this.getAllConcerts(this.state.city + '&' + this.state.inputText)})}}/>
+              <CurrentFilters date = {this.state.date} city = {this.state.city} onClick = {() => {
+                  this.setState({city : 'city=', date : new Date, reset : true}, () => {this.getAllConcerts(this.state.inputText)})}}/>
+            </div>
+           
+            <ConcertDisplay concerts = {this.state.concerts} date = {this.state.date}/>
             <Button>
               ' '
             </Button>
--- backend/frontend/src/components/searchbar.js
+++ backend/frontend/src/components/searchbar.js
@@ -1,23 +1,39 @@
 import React from 'react'
-import { TextField } from '@material-ui/core'
-import { Button } from '@material-ui/core';
+import { TextField, Autocomplete } from '@mui/material'
 
 
 function Searchbar(props) {
 
+    if (props.concerts){
+    const concerts = props.concerts
+    var composers = concerts.map((concert) => concert.composers)
+    composers = [].concat.apply([], composers)
+    composers = [...new Set(composers)]
+
+    var conductors = concerts.map((concert) => concert.conductor)
+    conductors = [...new Set(conductors)]
+    var search_suggestion = composers.concat(conductors)
+}
+
     return (
         <form noValidate autoComplete="off" 
                 onSubmit={props.onSubmit}>
-
-            <TextField variant = 'outlined' color = 'secondary' label = 'Search' onChange={props.onChange}
-                        type = 'text' value = {props.value} style = {props.style} size = 'medium'
-                        /* inputProps={{style: {fontSize: '1.5rem'}}} InputLabelProps={{style: {fontSize: '1.5rem'}}} */>
-
-                <Button color = 'primary' type = 'submit'>
-                    Click 
-                </Button>
-
-            </TextField>
+            {/* <TextField variant = 'outlined' color = 'secondary' onChange={props.onChange} label = 'Search'
+                        type = 'text' value = {props.value} style = {props.style} size = 'medium'/> */}
+            <Autocomplete
+                disableClearable
+                options={search_suggestion ? search_suggestion.map((option) => {return(option)}): ''}
+                renderInput={(params) => (
+                <TextField
+                    {...params}
+                    label="Search"
+                    InputProps={{
+                    ...params.InputProps,
+                    type: 'search',
+                    }}
+                />
+                )}
+            />
         </form>
     )
 }
