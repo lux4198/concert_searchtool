85c4056024f493fdc7c91fc73c0c64f909edb3e7
Lukas Nittka <lukas.nittka@gmail.com>
2022-01-20 16:04:44 +0100

    search for multiple values possible

--- backend/frontend/src/components/searchbar.js
+++ backend/frontend/src/components/searchbar.js
@@ -4,18 +4,25 @@ import { TextField, Autocomplete } from '@mui/material'
 
 function Searchbar(props) {
 
-    const [value, setValue] = useState('')
-
     if (props.concerts){
     const concerts = props.concerts
     var composers = concerts.map((concert) => concert.composers)
     composers = [].concat.apply([], composers)
     composers = [...new Set(composers)]
+    composers = composers.filter(composer => typeof(composer) === 'string')
+
 
     var conductors = concerts.map((concert) => concert.conductor)
     conductors = [...new Set(conductors)]
-    var search_suggestion = composers.concat(conductors)
+
+    var artists = concerts.map((concert) => concert.musicians)
+    artists = artists.map((artist) => Object.keys(artist))
+    artists = [].concat.apply([], artists)
+    artists = [...new Set(artists)]
+
+    var search_suggestion = composers.concat(conductors, artists)
     search_suggestion = [...new Set(search_suggestion)]
+
 }
 
     return (
@@ -24,6 +31,8 @@ function Searchbar(props) {
         //     <TextField variant = 'outlined' color = 'secondary' onChange={props.onChange} label = 'Search'
         //                 type = 'text' value = {props.value} style = {props.style} size = 'medium'/>
             <Autocomplete
+                multiple
+                freeSolo
                 disableClearable
                 options={search_suggestion ? search_suggestion.map((option) => {return(option)}): ''}
                 onChange = {(event, value) => {props.onSubmit(value)}}
@@ -31,6 +40,7 @@ function Searchbar(props) {
                 <TextField
                     {...params}
                     label="Search"
+                    placeholder='Search for Composer, Conductor, Artists'
                     InputProps={{
                     ...params.InputProps,
                     type: 'search',
