3226d0b24bc57ebad6c78f8d7a90c55b6c06a148
Lukas Nittka <lukas.nittka@gmail.com>
2022-01-21 10:57:41 +0100

    key removed

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x_016isec^lbtjd+28ijzv$(4@y0-nv9mwgdknf#phauqxbw$v'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'django.contrib.postgres', 
     'django_filters',
     'rest_framework', 
     'searchtool.apps.SearchtoolConfig',
