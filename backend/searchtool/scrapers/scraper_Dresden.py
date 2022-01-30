a6c060e2a6d3048eb6c2740ea3e5566258102490
Lukas Nittka <lukas.nittka@gmail.com>
2022-01-30 13:16:07 +0100

    added concert_date to singleevent

--- backend/searchtool/scrapers/scraper_Dresden.py
+++ backend/searchtool/scrapers/scraper_Dresden.py
@@ -1,3 +1,4 @@
+import json
 import requests
 from bs4 import BeautifulSoup
 from datetime import datetime 
@@ -7,17 +8,32 @@ from datetime import datetime
 import pytz
 from locale import setlocale, LC_TIME
 
+# download website data from main link 
 data = requests.get('https://www.staatskapelle-dresden.de/konzerte/konzertkalender/')
+# parse data into Beautifulsoup
 soup = BeautifulSoup(data.text, 'html.parser')
 
+# from parsed data, extract each concert item
 for item in soup.find_all('div', {'class' : 'spielplan-item'}):
+
+    # singleevent variable stores info about concert item before passing it to Event model 
     singleevent = {}
 
+    # get the link to concert details from item 
     link = item.find('a', href=True)
     link = 'https://www.staatskapelle-dresden.de/' + link['href']
-
     singleevent['link'] = link
-    print(link)
+
+    # get json data from script tag of each concert item
+    json_data = item.find('script', {'type' : 'application/ld+json'}).get_text()
+    json_data = json.loads(json_data)
+    # extract concert Datetime
+    concert_date = json_data['startDate']
+    # create datetime object 
+    concert_date = datetime.strptime(concert_date, '%Y-%m-%dT%H:%M')
+
+    # add date to singleevent and make it timezone aware 
+    singleevent['datetime'] = pytz.timezone('Europe/Berlin').localize(concert_date)
 
     # Event.objects.create(
     #     date = singleevent['datetime'], 
