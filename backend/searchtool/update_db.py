419771df8e6e898586fff091df9215c9a4044c5a
Lukas Nittka <lukas.nittka@gmail.com>
2022-01-30 23:08:34 +0100

    added scraper_Dresden to update_db.py

--- backend/searchtool/update_db.py
+++ backend/searchtool/update_db.py
@@ -2,6 +2,8 @@ from .scrapers.scraper_Berlin import main as berlin_main
 from  .scrapers.scraper_Hamburg import main as hamburg_main
 from .scrapers.scraper_Munich import main as munich_main
 from .scrapers.scraper_Frankfurt import main as frankfurt_main
+from .scrapers.scraper_Dresden import main as dresden_main
+
 from .models import Event 
 
 # this script updates the database by deleting current entries and replacing them with new ones from the scraper scripts 
@@ -12,6 +14,7 @@ def main():
     hamburg_main()
     munich_main()
     frankfurt_main()
+    dresden_main()
 
 if __name__ == '__main__':
     main()
