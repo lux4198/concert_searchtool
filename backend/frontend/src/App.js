ef9c9e8830474c9c6fa8874594263d005b48ddcb
Lukas Nittka <lukas.nittka@gmail.com>
2022-02-07 22:23:39 +0100

    added state to app component

--- backend/frontend/src/App.js
+++ backend/frontend/src/App.js
@@ -1,5 +1,10 @@
 import {React, Component} from 'react'
 import DetailedSearch from './pages/DetailedSearch.js';
+import Home from './pages/Home.js';
+
+import moment from 'moment';
+import axios from 'axios'
+
 
 
 
@@ -7,12 +12,36 @@ class App extends Component {
   constructor(){
     super();
       this.state = {
-        
+        allConcerts : [], 
+        allQueryConcerts : [], 
+        inputText : '', 
+        city: 'city=', 
+        date : new Date(), 
       }
   }
+
+  componentDidMount(){
+    this.getAllConcerts(this.state.inputText)
+  }
+
+  // function responsible for making the api call to get the concert items matching the query  
+  getAllConcerts = (input) => {
+    // date is either specified date from datePicker or default new(Date), so today 
+    const date = 'date=' + moment(this.state.date).format('YYYY-MM-DD HH:mm')
+
+      axios.get('/api/events/?'+ date + '&' + input)
+    .then((response) => {
+      // console.log(response.data)
+      this.setState({allConcerts : response.data, allQueryConcerts : response.data})
+    })
+  };
+
     render(){
       return(
-        <DetailedSearch/>
+        <div id = 'App'>
+          <Home concerts = {this.state.allConcerts} />
+        </div>
+        // <DetailedSearch/>
       )}
 }
 
