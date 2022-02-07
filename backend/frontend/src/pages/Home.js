3864eb74603a348cee1c588f282879009123d6f4
Lukas Nittka <lukas.nittka@gmail.com>
2022-02-07 22:25:23 +0100

    added basic home screen + background color

--- backend/frontend/src/pages/Home.js
+++ backend/frontend/src/pages/Home.js
@@ -1,8 +1,39 @@
 import React from 'react'
+import {Typography} from '@mui/material'
+import SearchBar from '../components/searchbar'
 
-function Home() {
+const homeStyle = {
+    color : 'white', 
+    marginTop : '200px',  
+    display : 'flex', 
+    justifyContent : 'center', 
+    height : '100%', 
+}
+
+function Home(props) {
+
     return (
-        ''
+        <div id = 'home' style = {homeStyle}>
+            
+            <div id = 'main-content' style = {{'display' : 'flex', 'flexDirection' : 'column', 
+                                                'textAlign' : 'center', 'justifyContent' : 'space-evenly',
+                                                'width' : '60%'}}>
+                <Typography variant = 'h2' style = {{'marginBottom' : '30px'}}>
+                    Wer spielt 
+                    Beethoven ?
+                </Typography>
+                <Typography variant = 'h5' style = {{'marginBottom' : '30px'}}>
+                    Suchen Sie aus über 1000 klassichen Konzerten in ganz Deutschland. 
+                </Typography>
+                <div >
+                    <SearchBar label = {'Ensemble, Komponist, Dirigent'}
+                                concerts = {props.concerts}/>
+                </div>
+            </div>
+
+
+
+        </div>
     )
 }
 
