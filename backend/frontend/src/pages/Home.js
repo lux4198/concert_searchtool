93ba12666b52c0a2da24bd48a2d25af342ecc30b
Lukas Nittka <lukas.nittka@gmail.com>
2022-02-08 16:36:34 +0100

    composerArray shuffled at every render

--- backend/frontend/src/pages/Home.js
+++ backend/frontend/src/pages/Home.js
@@ -62,7 +62,19 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * max);
   }
 
+function shuffleArray(array) {
+for (var i = array.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+}
+}
+
 function Home(props) {
+
+    // composerArray now displays unique sequence for every render 
+    shuffleArray(composerArray)
     
     return (
         <div id = 'home' style = {homeStyle}>
