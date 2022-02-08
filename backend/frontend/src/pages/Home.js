b4afd40039e8dec9fc65dd3b9db3918e23da4bb2
Lukas Nittka <lukas.nittka@gmail.com>
2022-02-08 16:25:30 +0100

    added composer animation in home.css file

--- backend/frontend/src/pages/Home.js
+++ backend/frontend/src/pages/Home.js
@@ -1,32 +1,94 @@
 import React from 'react'
-import {Typography} from '@mui/material'
+import {Typography, Box} from '@mui/material'
 import SearchBar from '../components/searchbar'
+import './Home.css'
+
+const composerArray = [
+    "Bernstein",
+    "Gershwin",
+    "Beethoven",
+    "Schostakowitsch",
+    "Brahms",
+    "Korngold",
+    "Grieg",
+    "Mozart",
+    "Bruckner",
+    "Strauss",
+    "Mahler",
+    "Schumann",
+    "Mendelssohn",
+    "Liszt",
+    "Haydn",
+    "Varèse",
+    "Rachmaninow",
+    "Strawinsky",
+    "Bach",
+    "Tschaikowsky",
+    "Schubert",
+    "Smetana",
+    "Puccini",
+    "Verdi",
+    "Dvořák",
+    "Prokofjew",
+    "Wagner",
+    "Schönberg",
+    "Ravel",
+    "Debussy",
+    "Bartók",
+    "Elgar",
+    "Sibelius",
+    "Weber",
+    "Vivaldi",
+    "Händel",
+    "Rossini",
+    "Bizet",
+    "Britten",
+    "Fauré",
+    "Skrjabin",
+    "Zemlinsky",
+    "Saint-Saëns",
+    "Mussorgsky",
+    "Gubaidulina"
+]
 
 const homeStyle = {
-    color : 'white', 
-    marginTop : '200px',  
+    marginTop : '60px',  
     display : 'flex', 
     justifyContent : 'center', 
     height : '100%', 
 }
 
-function Home(props) {
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
 
+function Home(props) {
+    
     return (
         <div id = 'home' style = {homeStyle}>
             
             <div id = 'main-content' style = {{'display' : 'flex', 'flexDirection' : 'column', 
                                                 'textAlign' : 'center', 'justifyContent' : 'space-evenly',
                                                 'width' : '60%'}}>
-                <Typography variant = 'h2' style = {{'marginBottom' : '30px'}}>
-                    Wer spielt 
-                    Beethoven ?
+                    <Typography variant = 'h2' style = {{'marginBottom' : '30px', 'textAlign' : 'start'}}>
+                        Wer spielt 
+
+                        <div class = 'rw-words rw-words-1'>
+                            {composerArray.map((composer, index) =>
+                                {   const delay = (index * 4).toString() + 's'
+                                    return(
+                                    <Typography color = 'secondary' variant = 'h2' style = {{'animationDelay' : delay, 'display' : 'inline', }}>
+                                        {composer}
+                                    </Typography>)}
+                                                    )}
+                        </div>
+
+                    </Typography>
+                <Typography variant = 'h5' style = {{'marginBottom' : '50px'}}>
+                    Suche aus über 1000 klassichen Konzerten in ganz Deutschland. 
                 </Typography>
-                <Typography variant = 'h5' style = {{'marginBottom' : '30px'}}>
-                    Suchen Sie aus über 1000 klassichen Konzerten in ganz Deutschland. 
-                </Typography>
-                <div >
-                    <SearchBar label = {'Ensemble, Komponist, Dirigent'}
+                <div style = {{'background' : '#fff', 'border' : 'solid #99BFD0'}}>
+                    <SearchBar label = {'Ensemble, Komponist, Dirigent, Stück'}
                                 concerts = {props.concerts}/>
                 </div>
             </div>
