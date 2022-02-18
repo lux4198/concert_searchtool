4b7970513538214201ded2743ccf1a4b95a43396
Lukas <lukas.nittka@posteo.de>
2022-02-18 10:49:20 +0100

    custom theme for datepicker

--- backend/frontend/src/components/Datepicker.js
+++ backend/frontend/src/components/Datepicker.js
@@ -3,25 +3,40 @@ import TextField from '@mui/material/TextField';
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
 import { DatePicker } from '@mui/lab';
+import { createTheme, ThemeProvider } from '@mui/material';
+
+const theme = createTheme({
+  components: {
+    MuiSvgIcon : {
+      styleOverrides: {
+        root: {
+          color: 'black', 
+        }
+      }
+    }
+  }
+});
 
 export default function MaterialUIPickers(props) {
   return (
     <div style = {{'background': 'white', 'borderRadius': '20px', 'marginLeft': '1rem', 'marginRight': '1rem'}}>
       <LocalizationProvider dateAdapter={AdapterDateFns}>
-          <DatePicker
+          <ThemeProvider theme = {theme}>
+            <DatePicker
             
-            OpenPickerButtonProps={{ style: { color: 'black' } }}
-            label="Datum"
-            inputFormat="MM/dd/yyyy"
-            value={props.value}
-            onChange={props.onChange}
-            renderInput={(params) => <TextField {...params}
-            sx={{
-              svg: { color : 'black' },
-              input: { color : 'black' },
-              label: { color : 'transparent' }, 
-            }}/>}
-          />
+              OpenPickerButtonProps={{ style: { color: 'black' } }}
+              label="Datum"
+              inputFormat="MM/dd/yyyy"
+              value={props.value}
+              onChange={props.onChange}
+              renderInput={(params) => <TextField {...params}
+              sx={{
+                svg: { color : 'black' },
+                input: { color : 'black' },
+                label: { color : 'transparent' },
+              }}/>}
+            />
+          </ThemeProvider>
       </LocalizationProvider>
     </div>
   );
