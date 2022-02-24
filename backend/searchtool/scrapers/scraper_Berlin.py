55b066d2a3bffb9ad86dd3cea0a0d078f9231c2f
Lukas Nittka <lukas.nittka@gmail.com>
2022-02-24 10:59:58 +0100

    changed scraper to include chamber music events

--- backend/searchtool/scrapers/scraper_Berlin.py
+++ backend/searchtool/scrapers/scraper_Berlin.py
@@ -9,13 +9,24 @@ setlocale(LC_TIME, 'de_DE')
 
 # from ..models import Event 
 
+# scraper looks at orchestra and chamber music events 
+
+
 # goes to details page of given concert and returns composers : pieces as dict 
 def get_concert_details(details_link):
     data = requests.get(details_link)
     soup = BeautifulSoup(data.text, 'html.parser')
     
     # lists every composer , piece as tuple (excludes empty divs)
-    pieces  = [(item.contents[0].text, item.contents[1].text) for item in soup.find_all('div', {'class' : 'piece'})if item.contents[0].text]
+    
+    pieces = []
+    for item in soup.find_all('div' , {'class' : 'piece'}):
+        if len(item.contents) == 2:
+            pieces.append((item.contents[0].text, item.contents[1].text))
+        elif len(item.contents) == 1:
+            pieces.append((item.contents[0].text, '')) 
+        else:
+            continue
 
     pieces_dict = {}
 
@@ -42,85 +53,95 @@ def date_march(concert_date):
 
 def main():
     concerts = []
-    month_list = ['2021-12', '2022-01', '2022-02', '2022-03', '2022-04', '2022-05', '2022-06', '2022-07']
-    # month_list = ['2022-03']
+    month_list = ['2022-02', '2022-03', '2022-04', '2022-05', '2022-06', '2022-07']
+
 
     for month in month_list:
         print(month, '\n')
+        urls = ['https://www.berliner-philharmoniker.de/konzerte/kalender/veranstaltungen/von/' + month + '/cat/ensemble/', 
+        'https://www.berliner-philharmoniker.de/konzerte/kalender/von/' + month]
 
-        data = requests.get('https://www.berliner-philharmoniker.de/konzerte/kalender/von/' + month)
-        soup = BeautifulSoup(data.text, 'html.parser')
+        for url in urls:
+            data = requests.get(url)
 
-        # find each concert in concert list 
-        for element in soup.find_all('article', { 'class' : 'calendar-entry clickable-box orchestra'}):
+            soup = BeautifulSoup(data.text, 'html.parser')
 
-            singleevent = {}
+            # find each concert in concert list 
+            for element in soup.find_all('article', { 'class' : 'calendar-entry clickable-box ensemble'}):
 
-            # get details from each concert 
-            concert_date = element.find('div', {'class' : 'performance-date'})
+                singleevent = {}
 
-            musicians = element.find_all(['h2', 'h3'], {'class' : ['main-musician', 'other-musician']})
-
-
-            # create datetime objects from concert_date - sample: Samstag,08. Jan 2022, 19.00 Uhr
-            concert_date = ' '.join(concert_date.text.split(' ')[:-1])
-
-            # convert Mär to Mrz so strptime can parse data for march
-            concert_date = date_march(concert_date)
-
-            singleevent['datetime'] = concert_date
-
-
-            # get musicians / orchester from musicians element 
-            singleevent['musicians'] = {}
-            for div in musicians:
-                musician = div.contents[0]
-                role = div.find('span', {'class' : 'role'})
-
-                if not role:
-                    if musician == 'Berliner Philharmoniker':
-                        continue
-                    singleevent['musicians'][musician] = ''
+                if urls.index(url) == 0:
+                    singleevent['ensemble'] = 'Kammermusik'
                 else:
-                    if not role.contents:
+                    singleevent['ensemble'] = 'Berliner Philharmoniker'
+
+                # get details from each concert 
+                concert_date = element.find('div', {'class' : 'performance-date'})
+
+                musicians = element.find_all(['h2', 'h3'], {'class' : ['main-musician', 'other-musician']})
+
+
+                # create datetime objects from concert_date - sample: Samstag,08. Jan 2022, 19.00 Uhr
+                concert_date = ' '.join(concert_date.text.split(' ')[:-1])
+
+                # convert Mär to Mrz so strptime can parse data for march
+                concert_date = date_march(concert_date)
+
+                singleevent['datetime'] = concert_date
+
+
+                # get musicians / orchester from musicians element 
+                singleevent['musicians'] = {}
+                singleevent['conductor'] = ''  
+                for div in musicians:
+                    musician = div.contents[0]
+                    role = div.find('span', {'class' : 'role'})
+
+                    if not role:
+                        if musician == 'Berliner Philharmoniker':
+                            continue
                         singleevent['musicians'][musician] = ''
-                        continue
-                    if role.contents[0] == ' Dirigent':
-                        singleevent['conductor'] = musician
-                        continue
-                    singleevent['musicians'][musician] = role.contents[0]
+                    else:
+                        if not role.contents:
+                            singleevent['musicians'][musician] = ''
+                            continue
+                        if role.contents[0] == ' Dirigent':
+                            singleevent['conductor'] = musician
+                            continue
+                        singleevent['musicians'][musician] = role.contents[0]
 
 
-            # create link that leads to specific concert 
-            details_link = element.find('a', {'class' : 'button grey read-more action-item'}, href = True)
-            details_link = 'https://www.berliner-philharmoniker.de/' + details_link['href']
+                # create link that leads to specific concert 
+                details_link = element.find('a', {'class' : 'button grey read-more action-item'}, href = True)
+                details_link = 'https://www.berliner-philharmoniker.de/' + details_link['href']
 
-            # safe link to singleevent 
-            singleevent['link'] = details_link
-
-
-            # get composers and pieces from details_link
-            composers_pieces = get_concert_details(details_link)
-            singleevent['composers'] = [item for item  in composers_pieces.keys()]
-            singleevent['pieces'] = [value for value in composers_pieces.values()]
+                # safe link to singleevent 
+                singleevent['link'] = details_link
 
 
-            # define the city of the events 
-            singleevent['city'] = 'Berlin'
-            singleevent['ensemble'] = 'Berliner Philharmoniker'
+                # get composers and pieces from details_link
+                composers_pieces = get_concert_details(details_link)
+                singleevent['composers'] = [item for item  in composers_pieces.keys()]
+                singleevent['pieces'] = [value for value in composers_pieces.values()]
 
-            # create entries in database for scraped data 
-            # Event.objects.create(
-            #     date = singleevent['date'], 
-            #     city = singleevent['city'], 
-            #     ensemble = singleevent['ensemble'], 
-            #     musicians = singleevent['musicians'], 
-            #     conductor = singleevent['conductor'],
-            #     composers = singleevent['composers'],
-            #     pieces = singleevent['pieces'],
-            #     link = singleevent['link'])
 
-            concerts.append(singleevent)
+                # define the city of the events 
+                singleevent['city'] = 'Berlin'
+                # print(singleevent)
+
+                # create entries in database for scraped data 
+                # Event.objects.create(
+                #     date = singleevent['date'], 
+                #     city = singleevent['city'], 
+                #     ensemble = singleevent['ensemble'], 
+                #     musicians = singleevent['musicians'], 
+                #     conductor = singleevent['conductor'],
+                #     composers = singleevent['composers'],
+                #     pieces = singleevent['pieces'],
+                #     link = singleevent['link'])
+
+                concerts.append(singleevent)
     return(concerts)
 
         
